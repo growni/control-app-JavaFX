89f63d91cc2fc1bd0ee257c9fff33c629e9e23ed
growni <sa6ko19961126@gmail.com>
2025-04-14 01:34:09 +0300

    Updated endpoints

--- src/main/java/com/example/control/utils/windows/ENDPOINTS.java
+++ src/main/java/com/example/control/utils/windows/ENDPOINTS.java
@@ -1,14 +1,14 @@
 package com.example.control.utils.windows;
 
 public enum ENDPOINTS {
-    LOGIN_URL("https://control-app-backend.onrender.com/api/auth/login"),
-    SEND_PASSWORD_RESET_LINK_URL("https://control-app-backend.onrender.com/api/auth/send-password-reset-link"),
-    FEEDBACK("https://control-app-backend.onrender.com/api/profile/feedback"),
-    IS_SUBSCRIBED("https://control-app-backend.onrender.com/api/profile/is-subscribed?email="),
-    SUBSCRIBE("https://control-app-backend.onrender.com/api/profile/subscribe"),
-    UNSUBSCRIBE("https://control-app-backend.onrender.com/api/profile/unsubscribe"),
-    REGISTER("https://control-app-backend.onrender.com/api/auth/register"),
-    UPDATE_PASSWORD("https://control-app-backend.onrender.com/api/profile/update-password");
+    LOGIN_URL("https://control-app-backend-aqm1.onrender.com/api/auth/login"),
+    SEND_PASSWORD_RESET_LINK_URL("https://control-app-backend-aqm1.onrender.com/api/auth/send-password-reset-link"),
+    FEEDBACK("https://control-app-backend-aqm1.onrender.com/api/profile/feedback"),
+    IS_SUBSCRIBED("https://control-app-backend-aqm1.onrender.com/api/profile/is-subscribed?email="),
+    SUBSCRIBE("https://control-app-backend-aqm1.onrender.com/api/profile/subscribe"),
+    UNSUBSCRIBE("https://control-app-backend-aqm1.onrender.com/api/profile/unsubscribe"),
+    REGISTER("https://control-app-backend-aqm1.onrender.com/api/auth/register"),
+    UPDATE_PASSWORD("https://control-app-backend-aqm1.onrender.com/api/profile/update-password");
 
     private final String url;
 
